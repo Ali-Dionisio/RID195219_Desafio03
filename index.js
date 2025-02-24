dfe6c0ce16f8b637f1edd359ddcbfb7c3b6a3f4b
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2025-02-24 20:13:18 -0300

    inclusão de novas tarefas e marcar como concluido

--- index.js
+++ index.js
@@ -3,102 +3,173 @@ const dados = [
     {id: 2, tarefa: 'Criar endpoint para cadastro de tarefas', etiqueta: 'backend', concluido: 'false'},
     {id: 3, tarefa: 'Implementar protótipo da listagem de tarefas', etiqueta: 'UX', concluido: 'true'},
 ]
+const contarTarefas = (dados) => {
+    let tarefasProgresso;
+    const tarefasProgressoDOM = document.getElementById('tarefas-progresso');
 
-const getConcluirBtn = (dado) => {
+    if (tarefasProgressoDOM) tarefasProgresso = tarefasProgressoDOM.value;
+    else {
+        const newTarefaProgressoDOM = document.createElement('div');
+        newTarefaProgressoDOM.id = 'tarefas-progresso';
+        document.getElementById('contarTarefas')
+                .appendChild(newTarefaProgressoDOM);
+        tarefasProgresso = newTarefaProgressoDOM;
+    }
+    const tarefasEncerradas = dados.filter(( {checked}) => checked).length
+    tarefasProgresso.textContent = `${tarefasEncerradas} tarefas concluídas`
+}
+
+const concluirTarefa = (buttonId) => {
+    dados.filter(({ id }) => parseInt(id) !== parseInt(buttonId));
+    const elemento = document.getElementById(buttonId);
+     const titulo = document.querySelector('#tit'+buttonId)
+     const botao = document.querySelector('#but'+buttonId);
+     const img = document.querySelector('#img'+buttonId);
+     elemento.id = `${buttonId} true`;
+     titulo.className = 'newTitulo';
+     botao.id = 'newBotao';
+     img.id = 'newImg';
+}
+
+const CriarListaTarefas = (dado, button) => {
+    // let concluido = dado.concluido || "false";
+    const now = new Date;
+    const data = (now)
+
+    const list = document.getElementById('tarefas');
+    const li = document.createElement('li');
+    
+    li.id = dado.id;
+    li.className = 'li';
+
+    const img = getImgInput(dado);
+
+    const subTitulo = document.createElement('p');
+    subTitulo.className = 'titulo';
+    // subTitulo.id = dado.concluido;
+    subTitulo.id = `tit${dado.id}`;
+    
+    const espacoTag_Dt = document.createElement('div');
+    espacoTag_Dt.className = 'espacoTag_Dt';
+    const espacoTag = document.createElement('div');
+    espacoTag.className = 'espacoTag';
+    espacoTag.id = dado.concluido;
     const espacoBotaoTag = document.createElement('div');
     espacoBotaoTag.className = 'espacoBotaoTag';
     espacoBotaoTag.id = dado.concluido;
 
-    const btnConcluir = document.createElement('button');
-    let img = document.createElement("img");
-    img.src= 'Checked.png';
-    img.className = 'checkedImg'
-    img.id = dado.concluido;
-    btnConcluir.className = 'btnConcluir';
-    btnConcluir.id = 'btnConcluir';
-    btnConcluir.textContent = "Concluir";
+    const tag = document.createElement('p');
+    tag.className = 'tag';
+    const dataTag = document.createElement('p');
+    dataTag.className = 'dataTag';
 
+    subTitulo.textContent = dado.tarefa || dado.nomeTarefa;
+    tag.textContent = dado.etiqueta;
+    dataTag.textContent = ('Criado em: ' + data.toLocaleDateString());
+
+    // contarTarefas();
+    // li.appendChild(button);
+    list.appendChild(li);
     li.appendChild(espacoBotaoTag)
-    espacoBotaoTag.appendChild(btnConcluir)
+    li.appendChild(espacoTag);
+    espacoTag.appendChild(subTitulo);
+    espacoTag.appendChild(espacoTag_Dt)
+    espacoTag_Dt.appendChild(tag);
+    espacoTag_Dt.appendChild(dataTag);
+    espacoBotaoTag.appendChild(button);
     espacoBotaoTag.appendChild(img)
 
+    return li;
+}
+
+const getImgInput = ({id, nomeTarefa, etiqueta, concluido}) => {
+    concluido = 'false';
+    const wrapper = document.createElement('div');
+    const imgId = `img${id}`;
+    const img = document.createElement("img");
+
+    img.src= 'Checked.png';
+    img.className = 'checkedImg'
+    img.id = imgId;
+
+    wrapper.appendChild(img);
+
+    return wrapper;
+}
+
+const getButtonInput = ({id, dado, concluido}) => {
+    concluido = 'true';
+    const button = document.createElement('button');
+    const wrapper = document.createElement('div');
+    const buttonId = `but${id}`;
+
+    button.type = "button";
+    button.id = buttonId
+    button.className = "btnConcluir";
+    button.textContent = "Concluir";
+
+    button.concluido = concluido || false;
+
+    button.onclick = () => 
+        concluirTarefa(id);
+
+    wrapper.appendChild(button);
+
+    return wrapper;
 }
 
 const getNovaTarefaId = () => {
     const lastId = dados[dados.length - 1]?.id;
-    return lastId ? lastId + 1 : 1;
+    let valor = lastId ? lastId + 1 : 1;
+    for (let i; i = valor; i++) {
+      return i;  
+    }
 }
+
 const getNovosDadosTarefa = (event) => {
+    const id = getNovaTarefaId();
     const nomeTarefa = event.target.elements.nomeTarefa.value;
     const etiqueta = event.target.elements.etiqueta.value;
 
-    return { nomeTarefa, etiqueta}
+    return { id, nomeTarefa, etiqueta}
 }
 
 const criarTarefa = (event) => {
     event.preventDefault();
-
     const newTarefaData = getNovosDadosTarefa(event);
-    const{id, nomeTarefa, etiqueta} = newTarefaData;
     
-    console.log(lastId);
-    console.log(nomeTarefa);
-    console.log(etiqueta);
-}
-window.onload = function () {
-    const form = document.getElementById('inputarDados');
-    form.addEventListener('submit',criarTarefa)
+    const button = getButtonInput(newTarefaData)
+    CriarListaTarefas(newTarefaData, button);
 
-    const now = new Date;
-    const data = (now)
+    dados : [
+        ...dados, 
+        {
+            id: newTarefaData.id, 
+            nomeTarefa: newTarefaData.nomeTarefa, 
+            etiqueta: newTarefaData.etiqueta,
+        }
+    ]
+}
+// const contadorTarefas = () => {
+//     const contarTarefas = document.getElementById('contadorTarefas');
+
+//     const contagemId = (dados.length) ;
+
+//     const campoContador = document.createElement('p');
+//     campoContador.textContent = 'Total tarefas: ' + contagemId;
+
+//     contarTarefas.appendChild(campoContador);
+
+// }
+
+window.onload = function() {
+    const form = document.getElementById('inputarDados');
+    form.addEventListener('submit', criarTarefa)
 
     dados.forEach((dado) => {
-
-        const list = document.getElementById('tarefas');
-        const li = document.createElement('li');
-        li.className = 'li';
-        li.id = 'qualities';
-
-        const subTitulo = document.createElement('p');
-        subTitulo.className = 'titulo';
-        subTitulo.id = dado.concluido;
-        
-        const espacoTag_Dt = document.createElement('div');
-        espacoTag_Dt.className = 'espacoTag_Dt';
-        const espacoTag = document.createElement('div');
-        espacoTag.className = 'espacoTag';
-        const espacoBotaoTag = document.createElement('div');
-        espacoBotaoTag.className = 'espacoBotaoTag';
-        espacoBotaoTag.id = dado.concluido;
-
-        const tag = document.createElement('p');
-        tag.className = 'tag';
-        const dataTag = document.createElement('p');
-        dataTag.className = 'dataTag';
-
-        const btnConcluir = document.createElement('button');
-        let img = document.createElement("img");
-        img.src= 'Checked.png';
-        img.className = 'checkedImg'
-        img.id = dado.concluido;
-        btnConcluir.className = 'btnConcluir';
-        btnConcluir.id = 'btnConcluir';
-        btnConcluir.textContent = "Concluir";
-
-        subTitulo.textContent = dado.tarefa;
-        tag.textContent = dado.etiqueta;
-        dataTag.textContent = ('Criado em: ' + data.toLocaleDateString());
-                
-
-        list.appendChild(li);
-        li.appendChild(espacoBotaoTag)
-        li.appendChild(espacoTag);
-        espacoTag.appendChild(subTitulo);
-        espacoTag.appendChild(espacoTag_Dt)
-        espacoTag_Dt.appendChild(tag);
-        espacoTag_Dt.appendChild(dataTag);
-        espacoBotaoTag.appendChild(btnConcluir)
-        espacoBotaoTag.appendChild(img)
-
+        const checkbox = getButtonInput(dado);
+        CriarListaTarefas(dado, checkbox)
     })
+    // contadorTarefas();
+    contarTarefas(dados)
 }
